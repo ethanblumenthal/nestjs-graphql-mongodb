dfd537408b6a11e13e41d39fe23b614b9a1ad8b4
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-03-31 11:55:47 -0500

    Implemented functionality for get student by id and get all students.

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './student.input';
 import { StudentService } from './student.service';
@@ -6,6 +6,16 @@ import { StudentService } from './student.service';
 @Resolver(of => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(returns => StudentType)
+  student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Query(returns => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation(returns => StudentType)
   createStudent(
