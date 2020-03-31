307de36d5f81ca916445401ee46bd4c01c80f5a4
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-03-31 10:39:37 -0500

    Setup graphql and first type.

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+  @Query(returns => LessonType)
+  lesson() {
+    return {
+      id: '12345',
+      name: 'Physics Class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
